8a1e9f317ec1026c8dd99f6e2f3d819e17e68ca4
shailentou <32923113+shailentou@users.noreply.github.com>
2021-01-27 13:21:16 +0530

    Add files via upload

---
+++ Zuulsvr/src/main/java/com/singh/shail/ZuulsvrApplication.java
@@ -0,0 +1,13 @@
+package com.singh.shail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulsvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulsvrApplication.class, args);
+	}
+
+}
