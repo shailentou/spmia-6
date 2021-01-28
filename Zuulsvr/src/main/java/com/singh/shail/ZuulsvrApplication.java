3f021ef8eaf99fec96bac1eca89b186fc597a5f0
shailentou <shailentou@gmail.com>
2021-01-28 12:11:17 +0530

    commit

--- Zuulsvr/src/main/java/com/singh/shail/ZuulsvrApplication.java
+++ Zuulsvr/src/main/java/com/singh/shail/ZuulsvrApplication.java
@@ -2,8 +2,10 @@ package com.singh.shail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulsvrApplication {
 
 	public static void main(String[] args) {
